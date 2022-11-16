98ad7e818b49ca17449d947cd0499350bdfe2f45
Keren Rogel <kerenrogel@gmail.com>
2022-11-16 10:28:38 +0800

    Update tailwindcss to Dawn theme

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'x',
+  content: [
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+  ],
+  theme: {
+    screens: {
+      'md': '750px',
+      'lg': '990px',
+    },
+    extend: {
+      fontFamily: {
+        'heading': 'var(--font-heading-family)'
+      },
+    },
+  },
+  plugins: [],
+}
